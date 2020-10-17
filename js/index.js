8774fe9fc00e5f241b0e713adea442019b9b01ca
Bermarte <bernardo.martelli@gmail.com>
2020-10-17 16:08:32 +0200

    Adding scrolling functionality

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
-$(function(){
-    $("[data-trigger").on("click", function(){
-        var targeet_id = $(this).attr('data-trigger');
-        $(targeet_id).toggleClass("show");
+$(function () {
+    $("[data-trigger").on("click", function () {
+        var target_id = $(this).attr('data-trigger');
+        $(target_id).toggleClass("show");
         $('body').toggleClass("offcanvas-active");
     });
 
     // close button
 
-    $(".btn-close").click(function(e){
+    $(".btn-close").click(function (e) {
         $(".navbar-collapse").removeClass("show");
         $("body").removeClass("offcanvas-active");
 
-    })
-})
+    });
+
+    $("#round-scroller").click(function (e) {
+        var target = $('#monster');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 500);
+            return false;
+        }
+    });
+
+});
